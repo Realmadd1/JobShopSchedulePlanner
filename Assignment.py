dedd2d88982271914b1f8e8e5d9eda49629eebf5
add1 <1206841441@qq.com>
2024-03-12 21:34:46 +0800

    feat:修改部分格式

--- Assignment.py
+++ Assignment.py
@@ -5,20 +5,20 @@ import datetime
 
 class Assignment:
     def __init__(self):
-        self.assignmentId: str                                              # 任务id
-        self.resourceId: str                                                # 机器id
-        self.productId: str                                                 # 产品id
-        self.productStepId: str                                             # 对应的productStepId
-        self.operationType: Optional[str] = None                            # 工序类型
-        self.startTime: datetime = datetime.datetime(2000, 1, 1, 0, 0, 0)            # 开始加工时间
-        self.endTime: datetime = datetime.datetime(3000, 1, 1, 0, 0, 0)              # 结束加工时间
-        self.prefixStartTime: datetime = datetime.datetime(2000, 1, 1, 0, 0, 0)      # 换型开始时间
-        self.prefixDuration: datetime.timedelta = datetime.timedelta(0)     # 换型时间 (默认为0)
-        self.prefixEndTime: datetime = datetime.datetime(3000, 1, 1, 0, 0, 0)        # 换型结束时间
-        self.processStartTime: datetime = datetime.datetime(2000, 1, 1, 0, 0, 0)     # 开始生产时间
-        self.processDuration: datetime.timedelta = datetime.timedelta(0)    # 生产时间
-        self.processEndTime: datetime = datetime.datetime(3000, 1, 1, 0, 0, 0)       # 结束生产时间
-        self.previousAssignment: Optional["Assignment"] = None              # 这个任务在该设备上的前一个任务
+        self.assignmentId: str                                                          # 任务id
+        self.resourceId: str                                                            # 机器id
+        self.productId: str                                                             # 产品id
+        self.productStepId: str                                                         # 对应的productStepId
+        self.operationType: Optional[str] = None                                        # 工序类型
+        self.startTime: datetime = datetime.datetime(2000, 1, 1, 0, 0, 0)               # 开始加工时间
+        self.endTime: datetime = datetime.datetime(3000, 1, 1, 0, 0, 0)                 # 结束加工时间
+        self.prefixStartTime: datetime = datetime.datetime(2000, 1, 1, 0, 0, 0)         # 换型开始时间
+        self.prefixDuration: datetime.timedelta = datetime.timedelta(0)                 # 换型时间 (默认为0)
+        self.prefixEndTime: datetime = datetime.datetime(3000, 1, 1, 0, 0, 0)           # 换型结束时间
+        self.processStartTime: datetime = datetime.datetime(2000, 1, 1, 0, 0, 0)        # 开始生产时间
+        self.processDuration: datetime.timedelta = datetime.timedelta(0)                # 生产时间
+        self.processEndTime: datetime = datetime.datetime(3000, 1, 1, 0, 0, 0)          # 结束生产时间
+        self.previousAssignment: Optional["Assignment"] = None                          # 这个任务在该设备上的前一个任务
 
     def calcStartTime(self):
         self.startTime = self.prefixStartTime
@@ -45,6 +45,4 @@ class Assignment:
         self.calcProcessEndTime()
         self.calcEndTime()
 
-
-assignment = Assignment()
 
